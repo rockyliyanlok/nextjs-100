529d71f5369e662df946d5113f86cccb04201192
Rocky Li <rocky.li@scmp.com>
2020-10-01 00:26:57 +0800

    i11-create-manifest-json-file

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,13 @@ const Index = () => {
       <Head>
         <title>Next.js 100</title>
         <meta name="Description" content="Build a PWA with Next.js to achieve 100 lighthouse score."></meta>
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+        <meta name="theme-color" content="#317EFB"/>
       </Head>
       <div>
         Next.js 100
