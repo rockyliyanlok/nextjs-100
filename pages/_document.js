fcf35e14cdf73521aabb463b1e618cb3fa610863
Yanlok Li <yli@bol.com>
2021-11-03 10:04:39 +0100

    i33-optimize-favicon-with-gzip-compression

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
           <meta name="Description" content="Build a PWA with Next.js to achieve 100 lighthouse score."></meta>
           <meta name="theme-color" content="#317EFB"/>
 
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
           <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
